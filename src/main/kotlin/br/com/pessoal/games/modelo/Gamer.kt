42f672fc85fa6360f2248834ee690285adeae52a
marquisalexander <marquis.alexander@outlook.com>
2024-02-28 19:38:49 -0300

    feature: loop no programa

--- src/main/kotlin/br/com/pessoal/games/modelo/Gamer.kt
+++ src/main/kotlin/br/com/pessoal/games/modelo/Gamer.kt
@@ -1,5 +1,6 @@
 package br.com.pessoal.games.modelo
 
+import org.example.br.com.pessoal.games.modelo.Jogo
 import kotlin.random.Random
 
 data class Gamer(var nome: String, var email: String) {
@@ -13,6 +14,8 @@ data class Gamer(var nome: String, var email: String) {
         }
     var idInterno: String? = null
         private set
+
+    val jogosBuscados = mutableListOf<Jogo>()
 
     constructor(nome: String, email: String, dataNascimento: String, usuario: String) : this(nome, email) {
         this.dataNascimento = dataNascimento
